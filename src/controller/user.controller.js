bfc2b4378d1a09454a5c5b319dbd217f754462a0
Swapno0 <swapno2003@gmail.com>
2025-05-20 09:27:12 +0600

    making registerUser controller without entry to database

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,14 +1,61 @@
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import path from 'path';
+import {ApiError} from "../utils/ApiError.js"
+import {uploader} from "../utils/cloudinary.js"
+import { ApiResponse } from '../utils/ApiResponse.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const registerUser = (async (req,res) => {
-    const {username,email,bio,password} = req.body
+    // Taking some input from the frontEnd through req.body
+    const {userName,email,bio,password} = req.body
     
+    // Validating the inputs
+    if (userName === "") {
+        throw new ApiError(400, "userName is required")
+    }
+    if (email === "") {
+        throw new ApiError(400, "email is required")
+    }
+    if (password === "") {
+        throw new ApiError(400, "password is required")
+    }
     
+    // Checking if user exists
+    // Check this using Oracle.
+
+
+    // we access and use files using multer.
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+
+    //validation again for required files.
+    if (!avatarLocalPath) {
+        throw new ApiError(400,"Avatar is needed")
+    }
+    if (!coverImageLocalPath) {
+        throw new ApiError(400,"CoverImage is needed")
+    }
+
+    // upload the files in cloudinary
+    const storedAvatar = await uploader(avatarLocalPath)
+    const storedCoverImage = await uploader(coverImageLocalPath)
+
+    // Do database entry the above informations.
+
+
+    // Check for user creation. Apatoto onnanno values diye dei.
+    const createdUser = {userName,email,bio,password,storedAvatar:storedAvatar.url,storedCoverImage:storedCoverImage.url}
+
+    // return response
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User registered successfully")
+    )
+
+
+
     
 })
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,7 +1,7 @@
 import { v2 } from "cloudinary"
 import fs from "fs"
 
-cloudinary.config({
+v2.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET
@@ -11,9 +11,11 @@ cloudinary.config({
 const uploader = async (localFilePath) => {
     try {
         if (!localFilePath) return null
-        const response = await cloudinary.v2.uploader.upload(localFilePath, {
+        const response = await v2.uploader.upload(localFilePath, {
             resource_type : "auto"
         })
+        fs.unlinkSync(localFilePath)
+        return response
     } catch (error) {
         fs.unlinkSync(localFilePath)
         return null
